8862bf229d52561311c10dfbfbc34a4f5ef2a518
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-09-08 21:04:27 +0700

    Ввод данных, проверка условия (есть ли чётные числа, стоящие после нечётных)

---
+++ exercise1/exercise1.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Rus"); //Для вывода на русском языке
+
+	int a[10], n, f;
+
+	printf("Введите количество элмементов последовательности: ");
+	scanf("%d", &n);
+
+	//Ввод последовательнсоти
+	for (int i = 0; i < 10; i++)
+		scanf("%d", &a[i]);
+
+	f = 1;
+
+	for (int i = 0; i < 9 && f; i++) {
+		if (a[i] % 2 == 1 && a[i + 1] % 2 == 0)
+			f = 0;
+	}
+}
